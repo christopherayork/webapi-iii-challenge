792053a5501d7e605123ef977e71534a41615fed
Chris York <christopherayork1992@gmail.com>
2019-09-04 18:53:32 -0400

    Buggy!

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code away!
+const express = require('express');
+const userRouter = require('./routes/userRouter');
+const bodyParser = require('body-parser');
+const app = express();
+// app.use(express.json());
+app.use(bodyParser());
+
+const logger = (req, res, next) => {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}`
+  );
+  next();
+};
+app.use(logger);
+
+app.use('/api/users', userRouter);
+
+app.listen(5000, () => console.log("Server running on port 5000"));
---
+++ routes/userRouter.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const router = express.Router();
+const validateUserId = require('../validation/validateUserId');
+const validateUser = require('../validation/validateUser');
+const db = require('../users/userDb');
+
+router.use('/', validateUser);
+
+router.route('/')
+  .post((req, res) => {
+    // our validateUser middleware checks it for us already
+    db.insert(req.body)
+      .then(r => {
+        console.log(r);
+        res.status(201).json({ message: 'User successfully created' });
+      })
+      .catch(e => {
+        console.error(e, e.response);
+        res.status(500).json({ errorMessage: 'There was a problem creating the user' });
+      });
+  });
+
+router.use('/:id', validateUserId);
+
+router.route('/:id')
+  .get((req, res) => {
+    // our middleware should have set our found user to req.user
+    if(req.user) res.status(200).json({ user: req.user });
+    else res.status(500).json({ errorMessage: 'There was an error retrieving the user' });
+  })
+  .put(validateUserId, (req, res) => {
+    // same here
+    console.log(req.user);
+    let user = req.body;
+    if(!user) res.status(400).json({ error: 'You must provide user information to update with' });
+    else if(!user.name) res.status(400).json({ error: 'You must provide a user name to update' });
+    else if(!req.user) res.status(500).json({ errorMessage: 'There was an error finding the user' });
+    else db.update(req.params.id, user)
+      .then(r => {
+        console.log(r);
+        res.status(200).json({ message: 'User updated successfully' });
+      })
+      .catch(e => {
+        console.log(e);
+        res.status(500).json({ errorMessage: 'Failed to update user' });
+      });
+  })
+  .delete((req, res) => {
+    // already verified user
+    db.remove(req.params.id)
+      .then(r => {
+        console.log(r);
+        res.status(200).json({ message: 'User successfully deleted' });
+      })
+      .catch(e => {
+        console.error(e, e.response);
+        res.status(500).json({ message: 'Could not delete user' });
+      });
+  });
+
+
+module.exports = router;
