6f397e3c98df14649c5a55447258e012a10c862d
Chris York <christopherayork1992@gmail.com>
2019-09-05 19:09:58 -0400

    Deployment config

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // code away!
+require('dotenv').config();
 const express = require('express');
 const userRouter = require('./routes/userRouter');
 const bodyParser = require('body-parser');
@@ -16,4 +17,6 @@ app.use(logger);
 
 app.use('/api/users', userRouter);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`\n*** Server running on  http://localhost:${port} ***\n`));
